04e2b6b64c81f8b0f0b04b85624231f353f4fa56
Amos Stone <amos.stone@activenetwork.com>
2012-09-28 16:29:21 -0700

    trying new UI for wtouch remote. [NOTE: UI ONLY. NO FXN]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trails::Application.config.secret_token = '4579ffec412d283db9e5be9a3a7bb6b89031311d484e54b096de821f81e5497c7a61a10b9358fa1d45b22b0252c816d484dab1f8bfe81ca1b823d2a62cf45533'
